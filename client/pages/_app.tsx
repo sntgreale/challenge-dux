a98e9b554e7c13d5da690fc96192ef2519e19be7
sntgreale <sntg.reale@gmail.com>
2024-12-25 12:03:13 -0300

    App integration and app creation

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,4 +1,9 @@
+import "primereact/resources/themes/saga-blue/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import "primeflex/primeflex.css";
 import "@/styles/globals.css";
+
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
